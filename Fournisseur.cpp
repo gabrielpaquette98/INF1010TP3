64956bfff6d5497d592a44c479bca0c3524ebe6f
Vittorio <vittorio-passduss@outlook.com>
2018-02-16 10:27:05 -0500

    Premier jet Implémentation de la classe Fournisseur.cpp

--- Fournisseur.cpp
+++ Fournisseur.cpp
@@ -1,6 +1,72 @@
 /********************************************
 * Titre: Travail pratique #3 - Fournisseur.cpp
-* Date: 
-* Auteur:
+* Date: 16 février 2018
+* Auteur: 1894061 & 1899088
 *******************************************/
 #include "Fournisseur.h"
+
+Fournisseur::Fournisseur(const string&  nom, const string& prenom, int identifiant, const string& codePostal)
+    : Usager::Usager(nom, prenom, identifiant, codePostal)
+    , contenuCatalogue_ { }
+{
+    for (int i = 0; i < NIVEAUX_SATISFACTION; i++) 
+        satisfaction_.niveaux_[i] = 0;
+}
+
+vector<Produit*> Fournisseur::obtenirCatalogue() const
+{
+    return contenuCatalogue_;
+}
+
+Satisfaction Fournisseur::obtenirSatisfaction() const 
+{
+    return satisfaction_;
+}
+
+void Fournisseur::modifierSatisfaction(Satisfaction satisfaction)
+{
+    for (int i = 0; i < NIVEAUX_SATISFACTION; i++)
+        satisfaction_.niveaux_[i] = satisfaction.niveaux_[i];
+}
+
+void Fournisseur::noter(int appreciation)
+{
+    satisfaction_.niveaux_[appreciation]++;
+}
+
+void Fournisseur::ajouterProduit(Produit* produit)
+{
+    contenuCatalogue_.push_back(produit);
+}
+
+void Fournisseur::enleverProduit(Produit* produit)
+{
+    int positionProduit = -1;
+    for (int i = 0; i < contenuCatalogue_.size() ; i++)
+    {
+        if (contenuCatalogue_[i] == produit)
+            positionProduit = i;
+    }
+    if (positionProduit != -1)
+    {
+        int positionARetirer = positionProduit-1;
+        if (positionProduit == 1)
+            positionARetirer = 1;
+        if (positionARetirer == -1)
+            positionARetirer = 0;
+        contenuCatalogue_.erase(contenuCatalogue_.begin()+(positionARetirer));
+    }
+}
+
+ostream& operator<<(ostream & os, Fournisseur& fournisseur)
+{
+    os << static_cast<Usager>(fournisseur)
+        << "\t satisfaction: " << endl;
+    for (int i = 0; i < NIVEAUX_SATISFACTION; i++)
+    {
+        os << "\t \t" << "Niveau: " << i 
+            << fournisseur.obtenirSatisfaction().niveaux_[i] << endl;
+    }
+
+    return os;
+}
